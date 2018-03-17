a067bc2487f170c63c7977a7d0b109e60c1897ce
Contreras123456 <league@WTS19.attlocal.net>
2018-03-17 12:29:53 -0700

    Create a Cool Project
    Made Robot move with coding. We learn to make him move, turn, put pen
    down, and set speed.

---
+++ First_Project_/First_Project_.pde
@@ -0,0 +1,14 @@
+void setup(){
+size(800,600);
+background(21,21,21);
+ellipse(100,10,10,10);
+fill(1,200,50);
+triangle(10,30,20,30,40,15);
+rect(21,21,21,21);
+}
+void draw(){
+  background(10,30,90);
+if(mousePressed==true){
+ellipse(245,110,75,100);
+}
+}
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,16 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+Robot LOL=new Robot();
+LOL.move(100);
+LOL.penDown();
+LOL.turn(100);
+LOL.setSpeed(100);
+LOL.move(200);
+LOL.penDown();
+LOL.turn(200);
+LOL.setSpeed(200);
+LOL.move(300);
+LOL.turn(300);
 	}
 }
